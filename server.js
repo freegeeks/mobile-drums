0e0b30b261c2841c34a12c45c588f545f0687732
renasboy <renasboy@gmail.com>
2013-05-17 19:02:15 +0200

    Changes to server as discussed in TODO, not tested as usual :-D

--- server.js
+++ server.js
@@ -29,30 +29,59 @@ app.get('/hh2.wav', function (req, res) {
   res.sendfile(__dirname + '/wav/hh2.wav');
 });
 
+// Room needs to be after instruments
+// for precedence reasons
 app.get('/:room', function (req, res) {
   res.sendfile(__dirname + '/room.html');
 });
 
+// hold all adm for the rooms
+var adms = {};
+
 // On new connections (from admin or instrument)
 io.sockets.on('connection', function (socket) {
 
+    // once a new client joins the server
+    socket.on('join', function (room) {
+
+        // define role
+        var role = adms[room] ? 'cli' : 'adm';
+        
+        // join room
+        socket.join(room);
+
+        // tell the role
+        socket.emit('role', role);
+    });
+
     // When new instrument joins 
-	socket.on('instrument', function(data) {
-        // Report to admin
-		io.sockets.emit(data.room + '-instrument', { wav: data.wav });
-	});
+    socket.on('instrument', function(data) {
+
+        // if admin left and no new
+        // admin came in THIS IS BUG
+        // IN THE WAY WE SELECT NEW ADMIN
+        if (!adms[data.room]) {
+            return;
+        }
+
+        // Report to admin which one
+        io.sockets[adms[data.room]].emit('instrument', data.wav);
+    });
+
 
     // Once admin requests a HIT
 	socket.on('hit', function(data) {
         // Hit the instrument!
-		io.sockets.emit(data.room + '-hit-' + data.wav);
+		io.sockets.in(data.room).emit('hit', data.wav);
 	});
+});
 
-    // Client self discovery
-    socket.on('ping', function (data) {
-        io.sockets.emit('ping', { id: data.id, room: data.room });
-    });
-    socket.on('pong', function (data) {
-        io.sockets.emit('pong', { id: data.id, room: data.room });
-    });
+// clean adm list on disconnect
+// so next client will get to be the admin
+io.sockets.on('disconnect', function (socket) {
+    for (var room in io.sockets.manager.roomClients[socket.id]) {
+        if (adms[room.substring(1)] == socket.id) {
+            adms[room.substring(1)] == null;
+        }
+    }
 });
