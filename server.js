364e62c02ffaf5389df62f8615372185d21d197f
renasboy <renasboy@gmail.com>
2013-05-19 10:36:28 +0200

    Refactored last code added after a night of sleep, added hit to instruments to be visible

--- server.js
+++ server.js
@@ -98,36 +98,57 @@ io.sockets.on('connection', function (socket) {
 // pattern step, this is called
 // on each of the steps during pattern loop
 function pattern (data, step) {
-    // if there is anything to play
-    if (data.pattern[step % data.pattern.length]) {
-        // play all sounds at once
-        for (var i in data.pattern[step % data.pattern.length]) {
-            io.sockets.in(data.room).emit('hit', data.pattern[step % data.pattern.length][i]);
-        }
+
+    // get list of patterns for this step
+    var pattern = data.pattern[step % data.pattern.length];
+
+    // if nothing to play we JUMP jump jump! Everybody jump!
+    if (!pattern) {
+        return;
+    }
+    // play all sounds in this step at once
+    for (var i in pattern) {
+        // Hit the instrument!
+        io.sockets.in(data.room).emit('hit', pattern[i]);
     }
 }
 
 // This is called to update
 // stats at the admin side
 function stats (room) {
+
+    // this is the stats we return (emit)
     var stats   = {};
+
+    // get all sockets in this room
     var clis    = io.sockets.clients(room);
+
+    // loop through them
     for (var i in clis) {
+
+        // get data we assign to socket and do something in the callback
         clis[i].get('instrument', function (err, name) {
-            if (name) {
-                if (!stats[name]) {
-                    stats[name] = 1;
-                }
-                else {
-                    stats[name]++;
-                }
+
+            // only count if there is an instrument assigned
+            if (!name) {
+                return;
             }
+
+            // if first time we see this name ZERO it
+            if (!stats[name]) {
+                stats[name] = 0;
+            }
+            
+            // update stat counter
+            stats[name]++;
         })
     }
+
     // in case of no admin it breaks
     if (!adms[room]) {
         return;
     }
-    io.sockets.sockets[adms[room]].emit('instrument', stats);
 
+    // send STATS updated to admin
+    io.sockets.sockets[adms[room]].emit('instrument', stats);
 }
